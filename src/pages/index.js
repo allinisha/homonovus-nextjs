ff39bcd0cf66f63fbd916373c2883814609d8db3
homonovus <linzixiangis@126.com>
2024-08-07 21:54:25 +0800

    feat: 过审核

--- src/pages/index.js
+++ src/pages/index.js
@@ -103,7 +103,7 @@ export default function Home() {
 	return (
 		<main className={`flex min-h-screen flex-col items-center justify-between p-20`}>
 			<Head>
-				<title>"新人类"</title>
+				<title>"快乐生活lll"</title>
 			</Head>
 			{renderBackground()}
 			<div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
